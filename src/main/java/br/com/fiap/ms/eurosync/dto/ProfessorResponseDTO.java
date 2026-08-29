f30a3d8011576f440956ce8b76e46438ef4ec2a3
Renan Lopes <renanlmv12@gmail.com>
2026-08-29 20:36:17 -0300

    feat: define ordem de atributos no response

--- src/main/java/br/com/fiap/ms/eurosync/dto/ProfessorResponseDTO.java
+++ src/main/java/br/com/fiap/ms/eurosync/dto/ProfessorResponseDTO.java
@@ -1,6 +1,7 @@
 package br.com.fiap.ms.eurosync.dto;
 
 import br.com.fiap.ms.eurosync.entity.Professor;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonPropertyOrder({ "id", "nome", "email" })
 public class ProfessorResponseDTO {
 
     private Long id;
